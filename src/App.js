3a860b4e2289cd325c59e54c548db709e1bafb91
Piotr Dobrzyński <50482622+piotrdobrzynski@users.noreply.github.com>
2022-07-01 14:36:35 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import './Own.css';
 import Data from './data.json'
-//import PokemonThumb from './PokemonThumb'
-//import {useEffect, useState} from "react";
 
 function App() {
   const box = [];
@@ -21,52 +19,5 @@ return (
 );
 }
 
-// const App = () => {
-//
-//    const  [allPokemons, setAllPokemons] = useState([])
-//    const [loadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=500')
-//
-//   const getAllPokemons = async () => {
-//     const res = await fetch(loadMore)
-//     const data = await res.json()
-//
-//     setLoadMore(data.next)
-//
-//     function createPokemonObject(results)  {
-//       results.forEach( async pokemon => {
-//         const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
-//         const data =  await res.json()
-//         setAllPokemons( currentList => [...currentList, data])
-//         await allPokemons.sort((a, b) => a.id - b.id)
-//       })
-//     }
-//     createPokemonObject(data.results)
-//   }
-//
-//  useEffect(() => {
-//   getAllPokemons();
-//      // eslint-disable-next-line
-// }, []);
-//
-//   return (
-//     <div className="app-contaner">
-//       <h1>Pokemon Evolution</h1>
-//       <div className="pokemon-container">
-//         <div className="all-container">
-//           {allPokemons.map( (pokemonStats, index) =>
-//             <PokemonThumb
-//               key={index}
-//               id={pokemonStats.id}
-//               image={pokemonStats.sprites.other.dream_world.front_default}
-//               name={pokemonStats.name}
-//               type={pokemonStats.types[0].type.name}
-//             />)}
-//
-//         </div>
-//           <button className="load-more" onClick={() => getAllPokemons()}>Load more</button>
-//       </div>
-//     </div>
-//   );
-// }
 
 export default App;
